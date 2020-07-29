256bdb93ae126c0974d99c11cebefca8818a32a8
rehans-98 <60538802+rehans-98@users.noreply.github.com>
2020-07-29 23:52:52 +0530

    Update models.py

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -15,17 +15,3 @@ class Book(models.Model):
         self.cover.delete()
         super().delete(*args, **kwargs)
 
-
-class ref(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    pdf = models.FileField(upload_to='ref/pdfs/')
-    cover = models.ImageField(upload_to='ref/covers/', null=True, blank=True)
-
-    def __str__(self):
-        return self.title
-
-    def delete(self, *args, **kwargs):
-        self.pdf.delete()
-        self.cover.delete()
-        super().delete(*args, **kwargs)
